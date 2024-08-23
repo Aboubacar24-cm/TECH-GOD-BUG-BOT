95d2986691a87967b9b2929f2870ba25269a8d6f
Aboubacar24-cm <sukunaaa2007@gmail.com>
2024-08-23 12:12:34 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,20 +4,20 @@
 //Instagram: techgod143
 //Telegram: t.me/techgod143
 //GitHub: @techgod143
-//WhatsApp: +917466008456
+//WhatsApp: +2246600p2740
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@techgod143
 const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "224660002740"
+global.ownername = "🥱𝑺𝑼𝑲𝑼𝑵𝑨"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownernumber = '224660002740'  //creator number
+global.ownername = '🥱𝑺𝑼𝑲𝑼𝑵𝑨' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
